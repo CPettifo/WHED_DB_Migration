67a500b59096cd1ab911c261cea234c936810641
mbadar <mbadar@myune.edu.au>
2024-09-12 03:23:44 +1000

    Fixed and added year range check for a column

--- data_migration/cosc320_whed_org_staff_statistics_data.sql
+++ data_migration/cosc320_whed_org_staff_statistics_data.sql
@@ -1,3 +1,4 @@
+-- Perform the INSERT operation with year range check and default to 1901
 INSERT INTO cosc320_whed_org_staff_statistics (
     org_id, staff_statistics_year, is_statistics_approximate, 
     full_time_total, full_time_male, full_time_female,
@@ -7,11 +8,19 @@ INSERT INTO cosc320_whed_org_staff_statistics (
 SELECT 
     OrgID,
     CASE
-        WHEN iStaffStatisticsYear REGEXP '^[0-9]{4}$' THEN iStaffStatisticsYear
-        ELSE '0000'
+        WHEN iStaffStatisticsYear REGEXP '^[0-9]{4}$' AND 
+             CAST(iStaffStatisticsYear AS UNSIGNED) BETWEEN 1902 AND 2155 
+        THEN iStaffStatisticsYear
+        ELSE 1901  
     END AS staff_statistics_year,
-    iStaffStatisticsApprox != 0, 
-    iStaffFullTimeTotal, iStaffFullTimeMale, iStaffFullTimeFemale,
-    iStaffPartTimeTotal, iStaffPartTimeFemale, iStaffPartTimeMale,
-    iStaffDocFullTimeTotal, iStaffDocFullTimeMale, iStaffDocFullTimeFemale
+    COALESCE(iStaffStatisticsApprox != 0, FALSE), 
+    COALESCE(iStaffFullTimeTotal, 0), 
+    COALESCE(iStaffFullTimeMale, 0), 
+    COALESCE(iStaffFullTimeFemale, 0),
+    COALESCE(iStaffPartTimeTotal, 0), 
+    COALESCE(iStaffPartTimeFemale, 0), 
+    COALESCE(iStaffPartTimeMale, 0),
+    COALESCE(iStaffDocFullTimeTotal, 0), 
+    COALESCE(iStaffDocFullTimeMale, 0), 
+    COALESCE(iStaffDocFullTimeFemale, 0)
 FROM whed_org;
