15bc559d8a2bd329b8db402f850a1c38b2cc7d6c
mbadar <mbadar@myune.edu.au>
2024-09-16 23:41:27 +1000

    removed unnecessary quotation marks

--- data_migration/cosc320_whed_contact_data.sql
+++ data_migration/cosc320_whed_contact_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO `cosc320_whed_contact` 
+INSERT INTO cosc320_whed_contact 
 (contact_id, org_id, surname, first_name, job_title, job_function_code, years_of_office, phone, email, gender, is_current)
 SELECT 
   ContactID, 
